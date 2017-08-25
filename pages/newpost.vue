486ab1e39ee2a8cc5aeac630467fb16f928b8482
minhdtb <minhdtb.83@gmail.com>
2017-08-25 16:37:09 +0700

    update coding

---
+++ pages/newpost.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <h2>{{title}}</h2>
+                        <vue-markdown :source="content"></vue-markdown>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <input class="form-control" v-model="title" placeholder="Title..."/>
+                    </div>
+                </div>
+                <div class="row form-group">
+                    <div class="col-md-12">
+                        <textarea class="form-control" placeholder="Content..." v-model="content"
+                                  style="height: 200px"></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import VueMarkdown from 'vue-markdown'
+
+    export default {
+        middleware: 'auth',
+        components: {
+            VueMarkdown
+        },
+        data() {
+            return {
+                title: '',
+                content: ''
+            }
+        }
+    }
+</script>
--- pages/posts.vue
+++ pages/posts.vue
@@ -1,9 +1,21 @@
 <template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <nuxt-link v-if="isLogged" class="btn btn-default" to="/newpost">New Post</nuxt-link>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
     export default {
         fetch({store}) {
             return store.dispatch('GET_POSTS');
+        },
+        computed: {
+            isLogged: function () {
+                return !!this.$store.state.authUser;
+            }
         }
     }
 </script>
