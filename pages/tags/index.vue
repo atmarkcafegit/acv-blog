7cfbf32d0cdbe494ad8e5b78fba42ad92617e46a
minhdtb <minhdtb.83@gmail.com>
2017-09-11 16:47:56 +0700

    update coding

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container sitecontainer bgw">
         <div class="tags">
-            <nuxt-link v-for="tag in tags" :to="'/tags/' + tag._id"
+            <nuxt-link v-for="tag, i in tags" :to="'/tags/' + tag._id"
                        style="margin-left: 3px" :key="i">
                 {{tag._id}}
             </nuxt-link>
