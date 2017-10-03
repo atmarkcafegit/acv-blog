cb396f0bf0569d794d46d43a3a3546c8f953522a
minhdtb <minhdtb.83@gmail.com>
2017-10-03 09:40:25 +0700

    fix calculator

--- server/commons/utils.ts
+++ server/commons/utils.ts
@@ -1,5 +1,7 @@
 import * as express from 'express'
 import * as _ from 'lodash'
+import * as moment from 'moment'
+import {IUserModel} from "~/server/models/UserModel";
 
 export const PAGE_LIMIT = 5;
 
@@ -14,11 +16,13 @@ export const auth = (req: express.Request, res: express.Response, next: express.
     }
 };
 
-export const calcScore = (user, month) => {
+export const calcScore = (user: IUserModel, month: string) => {
     let postViews = 0;
 
     _.each(user.posts, post => {
-        postViews += (post as any).views;
+        if (moment(post.createdAt).format('YYYY-MM') === month) {
+            postViews += post.views;
+        }
     });
 
     let viewScore = Math.floor(postViews / 100);
